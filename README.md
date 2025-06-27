e5ef3977e7713c825bd47039ccd840dccc23494a
Adam Langbert <adamhl@pm.me>
2025-06-27 09:57:06 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# configs
+
+Configs (`tsconfig`, `biome`, etc) I use across my projects.
+
+```sh
+bun add -D @adamhl8/configs
+```
---
+++ configs/prettier.ts
@@ -0,0 +1,35 @@
+import type { Config } from "prettier"
+
+const prettierConfig: Config = {
+  printWidth: 120,
+  semi: false,
+  plugins: [
+    "prettier-plugin-sh",
+    "prettier-plugin-pkg",
+    "prettier-plugin-toml",
+    // "@prettier/plugin-xml",
+    "prettier-plugin-astro",
+    "@ianvs/prettier-plugin-sort-imports",
+    "prettier-plugin-tailwindcss",
+  ],
+  importOrder: ["<BUILTIN_MODULES>", "<THIRD_PARTY_MODULES>", "", "^@(/.*)$", "^[.]"],
+  importOrderTypeScriptVersion: "5.0.0",
+  tailwindStylesheet: "./src/global.css",
+  overrides: [
+    {
+      // https://github.com/prettier/prettier/issues/15956
+      files: "*.jsonc",
+      options: {
+        trailingComma: "none",
+      },
+    },
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+}
+
+export { prettierConfig }
