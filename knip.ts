4536674dfd2aad3952130cc488e5e3f9b91ed2d0
Adam Langbert <adamhl@pm.me>
2026-06-28 17:48:28 -0500

    update rules

--- knip.ts
+++ knip.ts
@@ -2,8 +2,6 @@ import { knipConfig } from "#/configs/knip.base.ts"
 
 const config = knipConfig({
   entry: ["./src/adamhl8-knip/knip-preprocessor.ts"],
-  ignoreBinaries: ["nub"],
-  ignoreFiles: ["patch-knip.ts"],
 } as const)
 
 export default config
--- oxlint.config.ts
+++ oxlint.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from "oxlint"
 
 import { oxlintConfig } from "#/index.ts"
 
-const config = oxlintConfig()
+const config = oxlintConfig({
+  rules: {
+    "sort-keys": ["error", "asc", { allowLineSeparatedGroups: true, natural: true }],
+  },
+})
 
 export default defineConfig(config)
